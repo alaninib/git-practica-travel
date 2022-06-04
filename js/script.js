9ff7108311228d65203211b06de9685a0188fe88
alaninib <alaninib.dev@gmail.com>
2022-06-04 15:07:37 -0400

    initial proyect

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict"
+
+const menuBarsBtn = document.getElementById("menu-bars");
+const navbar = document.querySelector(".navbar");
+
+const listener = (
+  () => {
+
+    menuBarsBtn.addEventListener("click", e => {
+      if(e.target.classList.contains("fa-bars")){
+        e.target.classList.remove("fa-bars");
+        e.target.classList.add("fa-times");
+        navbar.classList.add("active");
+      }else{
+        e.target.classList.remove("fa-times");
+        e.target.classList.add("fa-bars");
+        navbar.classList.remove("active");
+      }
+    })
+  }
+)()
